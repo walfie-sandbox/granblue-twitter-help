213e2ba7436fdbe6f7705c559883fb4c5dc33c6a
Walfie <walfington@gmail.com>
2016-06-26 00:02:18 -0400

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,11 @@
+scalaVersion := "2.11.8"
+
+organization := "com.github.walfie"
+
+name := "granblue-twitter"
+
+libraryDependencies ++= Seq(
+  "org.twitter4j" % "twitter4j-core" % "4.0.4",
+  "org.twitter4j" % "twitter4j-stream" % "4.0.4"
+)
+
---
+++ src/main/scala/com/github/walfie/granblue/twitter/Application.scala
@@ -0,0 +1,48 @@
+package com.github.walfie.granblue.twitter
+
+import twitter4j._
+import twitter4j.conf.ConfigurationBuilder
+
+// TODO: Make this not so bad
+object Application {
+
+  class StatusListener(twitter: Twitter) extends StatusAdapter {
+    val replacements = Seq(
+      "I need backup!" -> "参加者募集！",
+      "Battle ID: " -> "参戦ID：",
+      "Lvl " -> "Lv",
+      "Qinglong" -> "青竜",
+      "Xuanwu" -> "玄武",
+      "Baihu" -> "白虎",
+      "Zhuque" -> "朱雀",
+      "Agni" -> "アグニス",
+      "Neptune" -> "ネプチューン",
+      "Titan" -> "ティターン",
+      "Zephyrus" -> "ゼピュロス"
+    )
+
+    override def onStatus(status: Status): Unit = {
+      val original = status.getText
+
+      val replaced = replacements.foldLeft(original) {
+        case (acc, (before, after)) => acc.replaceAll(before, after)
+      }
+
+      if (original != replaced) {
+        twitter.updateStatus(new StatusUpdate(replaced))
+        twitter.destroyStatus(status.getId)
+      }
+    }
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    val twitter = TwitterFactory.getSingleton
+    val twitterStream = TwitterStreamFactory.getSingleton
+    val listener = new StatusListener(twitter)
+
+    twitterStream.addListener(listener)
+    twitterStream.user()
+  }
+}
+
